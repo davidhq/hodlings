f3578df901b8cf31589b471cefbb4bab343f62f9
David Krmpotic <david.krmpotic@gmail.com>
2021-01-16 13:34:15 +0100

    add --file option

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ import { homedir } from 'os';
 import program from 'commander';
 import renderHodlings from './lib/renderHodlings.js';
 
+function absolutizePath(path) {
+  return path.replace(/^~/, homedir());
+}
+
 program.version('0.8.0');
 program
   .option('-x, --convert <currency>', 'currency to display (usd, eur, cny...)', 'usd')
+  .option('--file <portfolio>', 'specify a custom hodlings file instead of defailt ~/.hodlings', '~/.hodlings')
   .option('--eth', 'focus on eth, hide the bitcoin-specific columns (value-btc, 7-day-change-vs-btc)')
   .option('--btc', 'focus on btc, hide the ethereum-specific columns (value-eth, 7-day-change-vs-eth)')
   .parse(process.argv);
 
 const baseCurrency = program.convert.toLowerCase();
-const hodlingsPath = path.join(homedir(), '.hodlings');
+const hodlingsPath = absolutizePath(program.file);
+
+const isDefaultHodlingsPath = path.join(homedir(), '.hodlings') == hodlingsPath;
+
 const focus = program.eth ? 'eth' : program.btc ? 'btc' : undefined;
 
-renderHodlings({ hodlingsPath, baseCurrency, focus });
+renderHodlings({ hodlingsPath, isDefaultHodlingsPath, baseCurrency, focus });
